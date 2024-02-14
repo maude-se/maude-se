d7ebbad9ea99cad803a57bce16db77f8bbbc546e
geunyeol.yu <rgyenr2@gmail.com>
2024-02-14 10:05:01 +0900

    update model print

--- src/py/hook/check.py
+++ src/py/hook/check.py
@@ -12,14 +12,12 @@ class CheckHook(Hook):
 
         # special symbol dict
         self._symbol_dict = {
-            # "int"     : "intAssnSymbol",
-            # "real"    : "realAssnSymbol",
-            # "bool"    : "boolAssnSymbol",
-            "assnPair": "assnPairSymbol",
-            "concat": "concatSatAssignmentSetSymbol",
-            "empty": "emptySatAssignmentSetSymbol",
-            "assn": "smtAssignmentResultSymbol",
-            "unknown": "unknownResultSymbol",
+            "assnPair"   : "assnPairSymbol",
+            "concat"     : "concatSatAssignmentSetSymbol",
+            "empty"      : "emptySatAssignmentSetSymbol",
+            "assn"       : "smtAssnResultSymbol",
+            "unsupModel" : "assnUnsupModelSymbol",
+            "unknown"    : "unknownResultSymbol",
         }
 
     def _get_symbol(self, name):
@@ -37,15 +35,29 @@ class CheckHook(Hook):
         empty_symbol = self._get_symbol("empty")
         top_module = empty_symbol.getModule()
 
+        assn_pair_sym = self._get_symbol("assnPair")
+        assn_unsup_sym = self._get_symbol("unsupModel")
+
         assn = empty_symbol.makeTerm([])
 
         for d in md:
-            v = self.conv.term2dag(d)
-            val = self.conv.term2dag(md[d])
+            try:
+                v = self.conv.term2dag(d)
+                val = self.conv.term2dag(md[d])
 
-            t = self._get_symbol("assnPair").makeTerm(
-                [top_module.upTerm(v), top_module.upTerm(val)]
-            )
+                t = assn_pair_sym.makeTerm(
+                    [top_module.upTerm(v), top_module.upTerm(val)]
+                )
+            except:
+                # contain some unsupported symbols
+                # e.g., uninterpreted
+                # hack
+                d0, _, _ = d
+                md0, _, _ = md[d]
+                v = top_module.parseTerm(f"\"{d0}\"")
+                val = top_module.parseTerm(f"\"{md0}\"")
+
+                t = assn_unsup_sym.makeTerm([v, val])
 
             assn = self._get_symbol("concat").makeTerm([assn, t])
         return self._get_symbol("assn").makeTerm([assn])
@@ -75,17 +87,12 @@ class CheckHook(Hook):
             # print(self.solv.assertions())
 
             if r == True and is_gen == tt:
-                try:
-                    assn = self._make_assn(module)
-                    return assn
-                except:
-                    # contain some unsupported symbols
-                    # e.g., uninterpreted
-                    return tt
+                assn = self._make_assn(module)
+                return assn
 
             # for printing purpose
-            if r == True and is_gen == ff:
-                self.conn.print_model()
+            # if r == True and is_gen == ff:
+                # self.conn.print_model()
 
             if r == True:
                 return tt
